7a955dbbc58fbad07e76c61d2b1f7c3ad2702a72
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-03-08 16:19:23 +0100

    adding admin panel

--- announcments_plugin/announcments_plugin.php
+++ announcments_plugin/announcments_plugin.php
@@ -7,4 +7,12 @@
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
+function naph_admin_actions(){ 
+    add_options_page("Announcements Post", "Announcements", 'manage_options', "naph_anon", "naph_admin_page_anon"); 
+} 
 
+function naph_admin_page(){
+?>
+  <h1>Newly Added Post Highlighter</h1>
+<?php
+}
