1cbb2fda5fadd2d6146f6ef15d787a55b1c12589
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-03-08 16:08:58 +0100

    baseline commit and plugin info

---
+++ announcments_plugin/announcments_plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: announcments_plugin
+ * Description: Insert your news before a post.
+ * Version: 1.0
+ * Author: Krzysztof Kulka, Tomasz Milewski
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
